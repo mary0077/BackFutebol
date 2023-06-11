e78e544c8b618ebd624b7b99b99bac0e819aba28
lanaclaudias <lcgs1@discente.ifpe.edu.br>
2023-06-11 14:50:21 -0300

    atualização

--- futebol.js
+++ futebol.js
@@ -4,14 +4,18 @@ const PORT = 3015;
 
 // Configuração para analisar JSON no corpo da requisição
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Rota para receber os dados do frontend via POST
-app.post('/', (req, res) => {
+app.post('/clube', (req, res) => {
+
+  
   const { nomeTime, jogadores, treinador, titulo, estadio, fundacao } = req.body;
 
   // Verifica se foram fornecidos todos os 6 dados
   if (nomeTime && jogadores && treinador && titulo && estadio && fundacao) {
     // Constrói a resposta HTML com os dados recebidos
+    app.get('/clube', (req, res) => {
     const responseHTML = `
       <h1>Dados Recebidos</h1>
       <p><strong>Nome do Time:</strong> ${nomeTime}</p>
@@ -21,9 +25,11 @@ app.post('/', (req, res) => {
       <p><strong>Estádio:</strong> ${estadio}</p>
       <p><strong>Fundação:</strong> ${fundacao}</p>
     `;
-
+    
     // Envia a resposta HTML ao navegador
+
     res.send(responseHTML);
+    });
   } else {
     // Caso não sejam fornecidos todos os 6 dados, envia uma mensagem de erro
     res.status(400).send('Erro: Todos os 6 dados devem ser fornecidos.');
